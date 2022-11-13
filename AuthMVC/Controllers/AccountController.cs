9e9fd4c7a83af09a15df1853e603c9106afcbe5c
Matthieu7924 <matthieugarban@gmail.com>
2022-11-13 15:19:07 +0100

    modif options password

--- AuthMVC/Controllers/AccountController.cs
+++ AuthMVC/Controllers/AccountController.cs
@@ -1,14 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AuthMVC.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AuthMVC.Controllers
 {
 	public class AccountController:Controller
 	{
+		//propriétés
+		private readonly UserManager<IdentityUser> userManager;
+		private readonly SignInManager<IdentityUser> signInManager;	
+
+
+		//constructeur
+		public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser>signInManager)
+		{
+			this.userManager = userManager;
+			this.signInManager = signInManager;
+		}
+
+
+
+
+
+
 		//GET /<controller>/
 		[HttpGet]
 		public IActionResult Register()
 		{
 			return View();
 		}
-	}
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+			if(ModelState.IsValid)
+			{
+				var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+				var result = await userManager.CreateAsync(user, model.Password);
+
+				if(result.Succeeded)
+				{
+					await signInManager.SignInAsync(user, isPersistent: false);//false pour session cookie // true pour permanent cookie
+					return RedirectToAction("index", "home");
+				}
+
+				foreach(var error in result.Errors)
+				{
+					ModelState.AddModelError(string.Empty, error.Description);
+				}
+			}
+            return View(model);
+        }
+
+
+
+    }
 }
--- AuthMVC/Program.cs
+++ AuthMVC/Program.cs
@@ -4,6 +4,11 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequiredLength = 10;
+});
+
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -28,6 +33,13 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
+
+
+
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
