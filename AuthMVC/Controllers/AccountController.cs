161c6725a8f3709f7c2dbb8b2d61ef719c0151a2
Matthieu7924 <matthieugarban@gmail.com>
2022-11-13 13:20:21 +0100

    captation registerAccount

---
+++ AuthMVC/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthMVC.Controllers
+{
+	public class AccountController:Controller
+	{
+		//GET /<controller>/
+		[HttpGet]
+		public IActionResult Register()
+		{
+			return View();
+		}
+	}
+}
